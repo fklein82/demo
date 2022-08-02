4b64b8296a44ab7a6588a4ca6ac7e912a923d938
fklein82 <frederic.klein@gmail.com>
2022-08-02 14:18:33 +0200

    first commit

---
+++ src/main/java/com/tanzu/fkleindemo/FkleinDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tanzu.fkleindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FkleinDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FkleinDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanzu/fkleindemo/FkleinDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanzu.fkleindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FkleinDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
